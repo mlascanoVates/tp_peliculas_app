f8b39d9cbe576dfccdf670683c1dd851877f58d2
mlascanoVates <mlascano@vates.com>
2023-10-25 13:13:27 -0300

    buscador

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SearchBar(){
-
+    const[input, setInput]= useState('');
     return (
         <div className="form-control m-3 p-10">
             <div className="font-medium  text-left tracking-wide" >
@@ -10,7 +10,16 @@ function SearchBar(){
                 <p></p>
             </div>
          
-            <input type="text" placeholder="Buscar" className="input input-bordered w-24 md:w-[100%]  " />
+             
+            <form onSubmit={e=>e.preventDefault()}   className="input input-bordered w-24 md:w-[90%] border-solid" >
+            <input onChange={(e)=>setInput(e.target.value)} 
+            type="text" placeholder="Buscar" 
+            className='md:w-[90%] '/>
+            <button className=" w-24  md:w-[10%]  border-1 h-10">
+              <Link to={`/results/${input}`}>🔎</Link>
+            </button>
+        
+          </form>
         </div>
     )
    }
